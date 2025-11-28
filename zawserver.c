07d4d101339435b92845c1334ef73c3377ed550c
zawadzki <zawadzki1@duck.com>
2025-11-29 00:02:21 +0100

    crash on binding fix + small additions

--- zawserver.c
+++ zawserver.c
@@ -5,15 +5,24 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/sendfile.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define LOG "\x1b[34m[  LOG  ]\x1b[0m"
 #define VERSION "infdev"
+
+/* DEBUG enables debug prints
+   VERBOSE enables regular logs
+   DUMP prints variables on crash */
+bool DEBUG = true;
+bool VERBOSE = true;
+bool DUMP = true;
 
 void print_log(const char log[]);
 void sigint_handler(int s);
@@ -22,10 +31,12 @@ void dump();
 void quit(int c);
 void sigpipe_handler(int s);
 char* type(char* ext);
+void dbg_print(const char* s);
 
 int sock, client_fd, opened_fd;
 FILE* fp = NULL;
 char buffer[256] = {0};
+unsigned int port = 80;
 
 int main(int argc, char* argv[]) {
     signal(SIGSEGV, segfault_handler);
@@ -34,24 +45,18 @@ int main(int argc, char* argv[]) {
     printf("argc: %d\nargv[0]: %s\nargv[1]: %s\nargv[2]: %s\n", argc, argv[0], argv[1], argv[2]);
 
     printf("This software is provded \"as is\" with absolutely no waranty\nZAWServer - Zawadzki's Anti-Windows (Software) Server %s 2025\nTo quit press \x1b[32mCtrl+C\x1b[0m\n\n", VERSION);
-
-    unsigned int port = 8080;
+     
     if (argv[1]) {
 	for (int i = 0; argv[1][i] != '\0'; i++) {
 	    if (!isdigit(argv[1][i])) return 2;
 	}
-	print_log("argument is digits!!");
+	dbg_print("argument is digits!!");
 	port = atoi(argv[1]);
 	printf("%s port set to %d\n", LOG, port);
     }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    /* struct sockaddr_in addr = {
-	AF_INET,
-	htons(port),
-	0
-    }; */
-    
+   
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
@@ -59,7 +64,7 @@ int main(int argc, char* argv[]) {
 
     print_log("initialized addr");
     int b = bind(sock, (struct sockaddr*)&addr, sizeof(addr));
-    if (b != 0) return 1;
+    /* if (b != 0) return 1; */
 
     printf("%s listening on port %d\n", LOG, port);
     listen(sock, 1);
@@ -123,17 +128,22 @@ int main(int argc, char* argv[]) {
     }
 }
 
-void print_log(const char log[]) { printf("\x1b[34m[  LOG  ]\x1b[0m %s\n", log); }
+void print_log(const char log[]) { 
+    if (VERBOSE)
+	printf("\x1b[34m[  LOG  ]\x1b[0m %s\n", log);
+}
 
 void sigint_handler(int s) {
-    print_log("caught SIGINT");
+    if (VERBOSE) print_log("caught SIGINT");
     quit(0);
 }
 
 void segfault_handler(int s) {
-    print_log("caught SIGSEGV\a");
-    dump();
-    quit(1);
+    if (DEBUG) print_log("caught SIGSEGV\a");
+    if (VERBOSE) print_log("error - segmentation fault");
+    if (DUMP) dump();
+    /* quit(1); */
+    abort();
 }
 
 void dump() {
@@ -165,3 +175,7 @@ char* type(char *ext) {
     return "text/raw";
 } 
 
+void dbg_print(const char* log) {
+    if (DEBUG)
+	printf("\x1b[31m[  DBG  ]\x1b[0m %s\n", log);
+}
